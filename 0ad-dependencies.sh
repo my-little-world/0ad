663a69b625541295495c64ab4a01e6c27eb28ab3
Joao V <29417278+joaovsq@users.noreply.github.com>
2023-05-07 16:17:13 -0400

    dependencies file

---
+++ 0ad-dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# use 'autoremove' instead of 'install' to remove the dependencies
+
+sudo dnf install boost-devel enet-devel fmt-devel gloox-devel libcurl-devel libicu-devel libpng-devel libsodium-devel \
+    libvorbis-devel libxml2-devel miniupnpc-devel openal-soft-devel \
+    SDL2-devel wxGTK3-devel 
+ 
